e3a9d887ddd2d1418cda499bf1b2ced53a44c8cd
konvita2 <konvita2@gmail.com>
2015-07-17 13:28:34 +0300

    curs model is added

---
+++ application/models/Curs_model.php
@@ -0,0 +1,31 @@
+<?
+
+class Curs_model extends CI_Model {
+
+	//constructor\
+	function __construct(){
+		parent::__construct();
+	}
+
+	//full list
+	/**
+	* get all rows ordered by nam, dat
+	*/
+	function get_all(){
+		
+
+	}
+
+	//get list by currency
+
+	//get curs by currency and dat
+
+	//add curs by currency and dat
+
+	//delete currency and dat
+
+	//test if exist curr-date row 
+
+}
+
+?>
